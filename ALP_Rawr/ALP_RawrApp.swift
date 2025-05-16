7797c027946c167706c96c9c9512d7edae3e7cd1
Felicia <feliciajoshlyn@gmail.com>
2025-05-16 09:21:54 +0700

    Initial Commit

---
+++ ALP_Rawr/ALP_RawrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_RawrApp.swift
+//  ALP_Rawr
+//
+//  Created by student on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ALP_RawrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
