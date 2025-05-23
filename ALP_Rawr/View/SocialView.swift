a19ea19ef0216b8a7bec5306daf53f7793100a33
feli <feliciajoshlyn@gmail.com>
2025-05-23 08:51:53 +0700

    coba

---
+++ ALP_Rawr/View/SocialView.swift
@@ -0,0 +1,18 @@
+//
+//  SocialView.swift
+//  ALP_Rawr
+//
+//  Created by student on 23/05/25.
+//
+
+import SwiftUI
+
+struct SocialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SocialView()
+}
