ef3507c1476bd31d41de07601a5343bb92e73558
feli <feliciajoshlyn@gmail.com>
2025-05-23 11:51:47 +0700

    Create EntryCard.swift

---
+++ ALP_Rawr/View/EntryCard.swift
@@ -0,0 +1,20 @@
+//
+//  EntryCard.swift
+//  ALP_Rawr
+//
+//  Created by student on 23/05/25.
+//
+
+import SwiftUI
+
+struct EntryCard: View {
+    var body: some View {
+        ZStack{
+            Rectangle()
+        }
+    }
+}
+
+#Preview {
+    EntryCard()
+}
