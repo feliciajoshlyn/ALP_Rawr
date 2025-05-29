ab9b253efdda12d5f168f117ce0864ea036e08f5
yohanajocelyn <153157374+yohanajocelyn@users.noreply.github.com>
2025-05-29 15:26:23 +0700

    change userService after adding rule in firestore

--- ALP_Rawr/Service/UserService.swift
+++ ALP_Rawr/Service/UserService.swift
@@ -12,7 +12,7 @@ class UserService{
     private let fbFirestore = Firestore.firestore()
     
     func createUserDocument(userId: String, email: String, username: String) async throws {
-        let userRef = fbFirestore.collection("diaryEntries").document(userId)
+        let userRef = fbFirestore.collection("users").document(userId)
         try await userRef.setData([
             "email": email,
             "username": username,
